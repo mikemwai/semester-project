0f7c8ff1f1e5e3f2ae6e602c52a60fc5f80fbe5d
Caleb Etemesi <etemesicaleb@gmail.com>
2022-01-13 11:29:20 +0300

    Clean up main

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -2,6 +2,8 @@ package com.main;
 
 import com.main.db.Db;
 import com.main.gui.Gui;
+//import com.main.intf.*;
+import com.main.Console;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,20 +14,12 @@ public class Main {
     private static final Logger LOGGER = LogManager.getLogger();
 
 
-    public Main() throws SQLException {
-    }
-
 
     public static void main(String[] args) {
-         // do something.
-        try {
-            Db db = new Db();
-        }catch (SQLException e){
-            // exit for no reason at all.
-            System.exit(1);
-        }
-	// write your code here
+
+        Console.main(args);
+	    // write your code here
         LOGGER.info("Welcome to ...., we are glad you are here.");
-        Gui.main(null);
+        //Gui.main(null);
     }
 }
