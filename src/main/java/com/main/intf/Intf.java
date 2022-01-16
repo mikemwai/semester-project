1eb0ad9cfc400f8bcf7a28416a6c8d61ecd52d73
venoliah <venoliah.kennedy@strathmore.edu>
2022-01-16 17:33:41 +0300

    help me implement cost

--- src/main/java/com/main/intf/Intf.java
+++ src/main/java/com/main/intf/Intf.java
@@ -7,8 +7,11 @@ import org.apache.logging.log4j.Logger;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
@@ -61,10 +64,13 @@ class Doctors {
 class Appointments {
 
 }
-
-class Labs {
+class Labs{
 
 }
+
+
+
+
 
 class Pharmacy {
 
