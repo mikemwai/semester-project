6c9d804d1d8478fc804b6d5277f73e0b4f4a544e
Caleb Etemesi <etemesicaleb@gmail.com>
2022-01-31 13:18:33 +0300

    Demo layout for welcome GUI
    Not final product
    
    Not even cool
    
    But a good baseline

--- src/main/java/com/main/gui/Gui.java
+++ src/main/java/com/main/gui/Gui.java
@@ -13,6 +13,7 @@ package com.main.gui;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Objects;
 
 
 /*
@@ -21,17 +22,15 @@ import java.awt.event.ActionListener;
 *  */
 public class Gui implements ActionListener {
 
-    private static JLabel enterLabel;
-    private static JTextField enterText;
-    private static JLabel passwordLabel;
-    private static JPasswordField encryptionText;
-    private static JButton enterButton;
-    private static JLabel welcome;
+    private static  JFrame frame;
+
+    private static  String username;
+    private static String password;
 
     public static void main(String[] args) {
-        JFrame frame=new JFrame("MAKINI HOSPITAL");//creating instance of JFrame
+        frame=new JFrame("MAKINI HOSPITAL");//creating instance of JFrame
 
-        frame.setSize(400,500);
+        frame.setSize(500,600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel =new JPanel();
@@ -46,59 +45,82 @@ public class Gui implements ActionListener {
     private static void placeComponents(JPanel panel) {
         panel.setLayout(null);
         JLabel enterLabel=new JLabel("Personnel");
-        enterLabel.setBounds(130,100,100,40);
+        enterLabel.setBounds(130,100,200,40);
         panel.add(enterLabel);
 
         JTextField enterText=new JTextField(30);
-        enterText.setBounds(130,100,100,40);
+        enterText.setBounds(230,100,100,40);
+        enterText.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // get username
+                username =  enterText.getText();
+            }
+        });
         panel.add(enterText);
 
-        JLabel exitLabel= new JLabel("EXIT");
-        exitLabel.setBounds(50,40,40,20);
-        panel.add(exitLabel);
 
         JLabel passwordLabel =new JLabel("Enter password");
-        passwordLabel.setBounds(30,50,100,25);
+        passwordLabel.setBounds(130,200,100,25);
         panel.add(passwordLabel);
 
         JPasswordField encryptionText=new JPasswordField(30);
-        encryptionText.setBounds(30,50,100,25);
+        encryptionText.setBounds(230,200,100,40);
+        encryptionText.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // get password
+                password = String.valueOf(encryptionText.getPassword());
+            }
+        });
         panel.add(encryptionText);
 
-        JButton enterButton=new JButton("CLICK");
-        enterButton.setBounds(30,50,100,25);
-        enterButton.addActionListener(new Gui());
+        JButton exiButton = new JButton("EXIT");
+        exiButton.setBounds(200,400,40,20);
+        exiButton.addActionListener(new CloseListener());
+        panel.add(exiButton);
+
+        JButton enterButton=new JButton("Login");
+        enterButton.setBounds(230,250,100,35);
+        JLabel welcome =new JLabel("Well welcome :) ");
+        welcome.setBounds(230,290,200,25);
+        panel.add(welcome);
+        enterButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (Objects.equals(password, "OOPs")) {
+                    welcome.setText("Welcome Admin");
+
+                }
+                else{
+                    welcome.setText("WE DON'T KNOW YOU");
+                }
+            }
+        });
         panel.add(enterButton);
 
-        JLabel welcome =new JLabel("Well welcome : ");
-        welcome.setBounds(30,50,100,25);
-        welcome.setText("How can we be of assistance:");
 
 
+
+    }
+    private static void exitFuntcion(){
+        frame.dispose();
 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        String personnel= enterText.getText();
-        String password=encryptionText.getText();
-
-       if (personnel.equals("Caleb") && password.equals("OOPs")){
-           welcome.setText("You're in !");
-       }
-       else if (personnel.equals("Mike") && password.equals("OOPs")){
-           welcome.setText("You're in");
-       }
-       else if (personnel.equals("Precious") && password.equals("OOPs")){
-           welcome.setText("You're in");
-       }
-       else if (personnel.equals("Venoliah") && password.equals("OOPs")){
-           welcome.setText("You're in");
-       }
-      else {
-          welcome.setText("Please exit .Authorized personel only!!!");
-       }
 
 
+    }
+}
+/// Close the GUI
+/// not the best way, and we should probably use this.dispose()
+/// but I'm lazy and don't feel like doing it.
+class CloseListener implements ActionListener{
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //DO SOMETHING
+        System.exit(1);
     }
 }
